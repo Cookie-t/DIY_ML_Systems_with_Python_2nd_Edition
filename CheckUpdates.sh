ddda6effee866ef4c44757d94d62897250516194
Michael Fan <michael@Michaels-MacBook-Pro.local>
2018-11-27 12:55:33 +0800

    update

---
+++ CheckUpdates.sh
@@ -0,0 +1,6 @@
+sudo pip install --upgrade pip
+sudo pip install --upgrade scikit-learn
+sudo pip install --upgrade tensorflow
+sudo pip install --upgrade matplotlib
+sudo pip install --upgrade pandas
+sudo pip install --upgrade gym
